ee64706c57f5ee1d88ac5be0733f5243486ef3de
Ivan <vanyademglo@gmail.com>
2024-10-15 14:59:18 +0300

    added CustomerRepository

---
+++ src/main/java/com/testtask/theraven/persistence/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.testtask.theraven.persistence.repository;
+
+import com.testtask.theraven.domain.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
