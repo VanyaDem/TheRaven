3e79eb6d6b7322211c6b33cc052906a05857fde0
Ivan <vanyademglo@gmail.com>
2024-10-15 14:13:54 +0300

    created the Customer entity class

---
+++ src/main/java/com/testtask/theraven/domain/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.testtask.theraven.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private Long created;
+
+    @Column
+    private Long updated;
+
+    @Column(name = "full_name", nullable = false, length = 50)
+    private String fullName;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(length = 14)
+    private String phone;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+}
