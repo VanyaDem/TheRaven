e2d6e943f6f97a492ef69c1c9f20bf8f5b6663ea
Ivan <vanyademglo@gmail.com>
2024-10-15 13:06:20 +0300

    init commit

---
+++ src/main/java/com/testtask/theraven/TheRavenApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.theraven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtask/theraven/TheRavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.theraven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
