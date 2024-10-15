218ca4a123f3bbdbd23394cb1c18803166d98b42
Ivan <vanyademglo@gmail.com>
2024-10-15 22:01:54 +0300

    hided removed customers

--- src/main/java/com/testtask/theraven/service/CustomerService.java
+++ src/main/java/com/testtask/theraven/service/CustomerService.java
@@ -14,13 +14,16 @@ public class CustomerService {
     private final CustomerRepository repository;
 
     public List<Customer> getAll() {
-        return repository.findAll();
-
+        return repository.findAll()
+                .stream()
+                .filter(Customer::getIsActive)
+                .toList();
     }
 
     public Customer getById(Long id) {
         return repository
                 .findById(id)
+                .filter(Customer::getIsActive)
                 .orElseThrow();
     }
 
@@ -32,7 +35,7 @@ public class CustomerService {
 
         var oldCustomer = repository.findById(id).orElseThrow();
 
-        constraintsCheck(id, oldCustomer, updatedCustomer);
+        emailCheck(oldCustomer.getEmail(), updatedCustomer.getEmail());
 
         oldCustomer.setUpdated(System.currentTimeMillis());
         oldCustomer.setFullName(updatedCustomer.getFullName());
@@ -42,19 +45,15 @@ public class CustomerService {
     }
 
 
-
     public void delete(Long id) {
         var customer = repository.findById(id).orElseThrow();
         customer.setIsActive(false);
-        repository.flush();
+        repository.save(customer);
     }
 
-    private void constraintsCheck(Long id, Customer old, Customer upd){
-        if(!old.getEmail().equals(upd.getEmail())){
+    private void emailCheck(String email, String dtoEmail){
+        if(!email.equals(dtoEmail)){
             throw new RuntimeException("Email can not be edited!"); //todo: create specific exception
-        }
-        if (!id.equals(upd.getId())){
-            throw  new RuntimeException("Id can not be edited!"); //todo: create specific exception
         }
     }
 }
