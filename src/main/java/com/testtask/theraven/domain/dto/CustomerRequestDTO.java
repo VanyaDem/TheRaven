7833ad0687bc2b1a495ec230bd9ea8cf0e5f72a6
Ivan <vanyademglo@gmail.com>
2024-10-19 21:43:01 +0300

    added @NotBlank annotation for email field

--- src/main/java/com/testtask/theraven/domain/dto/CustomerRequestDTO.java
+++ src/main/java/com/testtask/theraven/domain/dto/CustomerRequestDTO.java
@@ -25,11 +25,13 @@ public class CustomerRequestDTO extends AbstractCustomerDTO {
     @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters")
     private String fullName;
 
+
+    @NotBlank(message = "Email is mandatory and cannot be blank")
     @Pattern(regexp = "^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$", message = "Email should be valid")
     // I didn't use the @Email annotation because it limits email to 75 characters (the test task requirements say 100)
     @Size(min = 2, max = 100, message = "Email must be between 2 and 100 characters long")
     private String email;
 
-    @Pattern(regexp = "^\\+\\d{5,13}$", message = "Phone number must start with '+' followed by 5 to 13 digits")
+    @Pattern(regexp = "^\\+\\d{5,12}$", message = "Phone number must start with '+' followed by 5 to 13 digits")
     private String phone;
 }
