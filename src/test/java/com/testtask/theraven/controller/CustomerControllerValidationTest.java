48790097c640a61ad3a0859924981b64a41ca5de
Ivan <vanyademglo@gmail.com>
2024-10-19 21:41:29 +0300

    added tests of data validation

--- src/test/java/com/testtask/theraven/controller/CustomerControllerValidationTest.java
+++ src/test/java/com/testtask/theraven/controller/CustomerControllerValidationTest.java
@@ -1,6 +1,7 @@
 package com.testtask.theraven.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testtask.theraven.domain.dto.CustomerRequestDTO;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -34,33 +35,82 @@ public class CustomerControllerValidationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @SneakyThrows
+    @ParameterizedTest
+    @ValueSource(strings = {"", "    ", "V", "null", "the longer name"})
+    void addCustomer_throws_exception_if_fullName_is_not_valid(String name) {
+        fullNameValidation(() -> MockMvcRequestBuilders.post("/api/customers"), name);
+    }
 
     @SneakyThrows
     @ParameterizedTest
-    @ValueSource(strings = {"", "vanya", "@mail.com", "vanya@@gmail.com", "@", "the longer email"})
-    void addCustomer_throw_exception_if_email_is_not_valid(String email) {
+    @ValueSource(strings = {"", "vanya", "@mail.com", "vanya@@gmail.com", "@", "null", "the longer email"})
+    void addCustomer_throws_exception_if_email_is_not_valid(String email) {
         emailValidation(() -> MockMvcRequestBuilders.post("/api/customers"), email);
     }
 
     @SneakyThrows
     @ParameterizedTest
-    @ValueSource(strings = {"", "vanya", "@mail.com", "vanya@@gmail.com", "@", "the longer email"})
-    void updateCustomer_throw_exception_if_email_is_not_valid(String email) {
+    @ValueSource(strings = {"", "0991112233", "+3809", "+3809911122333", "+3809911122RT"})
+    void addCustomer_throws_exception_if_phone_is_not_valid(String phone) {
+        phoneValidation(() -> MockMvcRequestBuilders.post("/api/customers"), phone);
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @ValueSource(strings = {"", "    ", "V", "null", "the longer name"})
+    void updateCustomer_throws_exception_if_fullName_is_not_valid(String name) {
+        fullNameValidation(() -> MockMvcRequestBuilders.put("/api/customers/1"), name);
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @ValueSource(strings = {"", "vanya", "@mail.com", "vanya@@gmail.com", "@", "null", "the longer email"})
+    void updateCustomer_throws_exception_if_email_is_not_valid(String email) {
         emailValidation(() -> MockMvcRequestBuilders.put("/api/customers/1"), email);
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @ValueSource(strings = {"", "0991112233", "+3809", "+3809911122333", "+3809911122RT"})
+    void updateCustomer_throws_exception_if_phone_is_not_valid(String phone) {
+        phoneValidation(() -> MockMvcRequestBuilders.put("/api/customers/1"), phone);
+    }
+
+    @SneakyThrows
+    private void fullNameValidation(Supplier<MockHttpServletRequestBuilder> requestBuilderSupplier, String nameFromValueSource) {
+        var dto = createCustomerDto();
+        dto.setFullName(setValueOrGenerated(nameFromValueSource));
+        doRequest(requestBuilderSupplier, dto);
     }
 
     @SneakyThrows
     private void emailValidation(Supplier<MockHttpServletRequestBuilder> requestBuilderSupplier, String emailFromValueSource) {
         var dto = createCustomerDto();
         dto.setEmail(setValueOrGenerated(emailFromValueSource));
+        doRequest(requestBuilderSupplier, dto);
+    }
 
+    @SneakyThrows
+    private void phoneValidation(Supplier<MockHttpServletRequestBuilder> requestBuilderSupplier, String phoneFromValueSource) {
+        var dto = createCustomerDto();
+        dto.setPhone(phoneFromValueSource);
+        doRequest(requestBuilderSupplier, dto);
+    }
+
+    private void doRequest(Supplier<MockHttpServletRequestBuilder> requestBuilderSupplier, CustomerRequestDTO dto) throws Exception {
         mockMvc.perform(requestBuilderSupplier.get()
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectToJson(objectMapper, dto)))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
-    private String setValueOrGenerated(String emailFromValueSource) {
-        return (emailFromValueSource.equals("the longer email")) ? generateTheLongerEmail() : emailFromValueSource;
+    private static String setValueOrGenerated(String valueFromValueSource) {
+        return switch (valueFromValueSource) {
+            case "the longer name" -> generateStringWithLength(51);
+            case "the longer email" -> generateStringWithLength(100) + "@gmail.com";
+            case "null" -> null;
+            default -> valueFromValueSource;
+        };
     }
 }
